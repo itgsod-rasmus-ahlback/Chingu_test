014bdd1901bd447cf7078fb60d1ca6ebfc216ab2
Rasmus Ahlbäck <rasmus.ahlback@elev.it-gymnasiet.se>
2012-11-23 22:44:05 +0100

    Added cloud class and window caption

--- game.rb
+++ game.rb
@@ -3,17 +3,26 @@ require 'chingu'
 
 class Game < Chingu::Window
 
+
+
 	# Constructor
 	def initialize
-		super 
+		super
+		
 		self.input = {esc: :exit}
 		@background_image = Background.create
 		@player = Player.create
+		@cloud = Cloud.create
 	end
+
+	def needs_cursor?
+    	false
+  	end
+
 end
 
 class Player < Chingu::GameObject
-	has_traits :velocity
+	has_traits :velocity, :timer
 
 	# meta/constructor
 	def setup
@@ -24,11 +33,12 @@ class Player < Chingu::GameObject
 			holding_right: :right,
 			holding_up: :up,
 			holding_down: :down,
-			holding_space: :fire
+			holding_space: :fire,
 			}
 
 		@speed = 10
 		@angle = 0
+		@counter = 0
 	end
 
 	def fire
@@ -36,30 +46,21 @@ class Player < Chingu::GameObject
 	end
 
 	def left
-		unless @x - 20 <= 0
 		@angle -= 4.5
-		end
 	end
 
 	def right
-		unless @x + 20 >= 800
 		@angle += 4.5
-		end
 	end
 
 	def up
 		self.velocity_y += Gosu::offset_y(@angle, 0.5)
 		self.velocity_x += Gosu::offset_x(@angle, 0.5)
-		unless @y <= 0
-			
-		end
 	end
 
 	def down
 		self.velocity_x -= Gosu::offset_x(@angle, 0.5)
 		self.velocity_y -= Gosu::offset_y(@angle, 0.5)
-		unless @y >= 600
-		end	
 	end
 
 	def update
@@ -67,7 +68,18 @@ class Player < Chingu::GameObject
 		self.velocity_y *= 0.95
 		@x %= 800
 		@y %= 600
+
+
+		$window.caption = ("Game Of Clouds: FPS #{$window.fps}")
+
+		if @counter == 90
+			Cloud.create
+			@counter = 0
+		end
+
+		@counter += rand(0..1)
 	end
+
 
 end
 
@@ -76,7 +88,7 @@ class Background < Chingu::GameObject
 	def setup
 		@x = 800/2
 		@y = 600/2
-		@image = Gosu::Image["space.jpg"]
+		@image = Gosu::Image["ocean.png"]
 	end
 end
 
@@ -84,10 +96,28 @@ class Laser < Chingu::GameObject
 	has_traits :velocity, :timer
 	def setup
 		@image = Gosu::Image["shoot.png"]
-		self.velocity_y = Gosu::offset_y(@angle, 10)
-		self.velocity_x = Gosu::offset_x(@angle, 10)
+		self.velocity_y = Gosu::offset_y(@angle, 20)
+		self.velocity_x = Gosu::offset_x(@angle, 20)
 		after(1000) {self.destroy}
 	end
 end
 
+class Cloud < Chingu::GameObject
+	has_traits :velocity
+
+	def setup
+		@image = Gosu::Image["cloud.png"]
+		@y = -50
+		@angle = 180
+		@x = rand(1..800)
+		self.velocity_y = Gosu::offset_y(@angle, 1)
+	end
+
+	def update
+		if @y > 650
+			self.destroy
+		end
+	end
+
+end
 Game.new.show
