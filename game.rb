dbaf13489d8bf52e13b7e53695dfdf9341cfefee
Rasmus Ahlbäck <rasmus.ahlback@elev.it-gymnasiet.se>
2012-11-23 11:46:04 +0100

    added laser and angle for player and background

--- game.rb
+++ game.rb
@@ -5,9 +5,9 @@ class Game < Chingu::Window
 
 	# Constructor
 	def initialize
-		super
+		super 
 		self.input = {esc: :exit}
-
+		@background_image = Background.create
 		@player = Player.create
 
 	end
@@ -23,15 +23,22 @@ class Player < Chingu::GameObject
 			holding_left: :left,
 			holding_right: :right,
 			holding_up: :up,
-			holding_down: :down
+			holding_down: :down,
+			holding_space: :fire
 			}
 
 		@speed = 10
+		@angle = 0
+	end
+
+	def fire
+		Laser.create(x: self.x, y: self.y, angle: self.angle)
 	end
 
 	def left
 		unless @x - 20 <= 0
 		@x -= @speed
+		@angle -= 4.5
 		end
 	end
 
@@ -53,6 +60,28 @@ class Player < Chingu::GameObject
 		end	
 	end
 
+	def move
+		@y = @y_vel
+	end
+
+end
+
+
+class Background < Chingu::GameObject
+	def setup
+		@x = 800/2
+		@y = 600/2
+		@image = Gosu::Image["space.jpg"]
+	end
+end
+
+class Laser < Chingu::GameObject
+	has_traits :velocity
+	def setup
+		@image = Gosu::Image["shoot.png"]
+		self.velocity_y = Gosu::offset_y(@angle, 10)
+		self.velocity_x = Gosu::offset_x(@angle, 10)
+	end
 end
 
 Game.new.show
