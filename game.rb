159f4730d0f22d9e940ca9fb51cc4baa07a72c35
Rasmus Ahlbäck <rasmus.ahlback@elev.it-gymnasiet.se>
2012-11-23 10:23:49 +0100

    Added Player class

--- game.rb
+++ game.rb
@@ -3,11 +3,21 @@ require 'chingu'
 
 class Game < Chingu::Window
 
-	
 	# Constructor
 	def initialize
 		super
+		@caption = "lol"
 		self.input = {esc: :exit}
+		@player = Player.create
+	end
+end
+
+class Player < Chingu::GameObject
+
+	# meta/constructor
+	def setup
+		@x, @y = 350, 400
+		@image = Gosu::Image["./final_ship.png"]
 	end
 end
 
